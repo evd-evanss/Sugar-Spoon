825bc7e50c2e9d2240a91a1c682087c62b88750d
evcostt <evandro.ribeiro@mobile2you.com.br>
2020-07-11 22:42:08 -0300

    add extension for visibility

--- app/src/main/java/com/sugarspoon/extensions/ViewsExtensions.kt
+++ app/src/main/java/com/sugarspoon/extensions/ViewsExtensions.kt
@@ -1,7 +1,10 @@
 package com.sugarspoon.extensions
 
+import android.view.View
+import android.view.View.*
 import android.widget.EditText
 
+//EditText
 fun EditText.text(text: String) {
     this.setText(text)
 }
@@ -14,4 +17,12 @@ fun EditText.hintPassword() {
     this.hint = "******"
 }
 
+//GenericViews
+fun View.setVisibility(isVisible: Boolean?) {
+    when(isVisible) {
+        true -> this.visibility = VISIBLE
+        false -> this.visibility = INVISIBLE
+        else -> this.visibility = GONE
+    }
+}
 
